21287a410b98e7d65edc80824ce776e076ded90c
Evgenii Dianov <dianov.js@gmail.com>
2021-08-17 12:47:07 +0300

    initial

---
+++ gulp/tasks/html.js
@@ -0,0 +1,14 @@
+import gulp from 'gulp'
+import fileinclude from 'gulp-file-include'
+import gulpif from 'gulp-if'
+import plumber from 'gulp-plumber'
+import config from '../config'
+
+export const htmlBuild = () =>
+ gulp
+  .src(`${config.src.html}`)
+  .pipe(plumber())
+  .pipe(fileinclude())
+  .pipe(gulp.dest(`${config.dest.html}`))
+
+export const htmlWatch = () => gulp.watch(`${config.src.html}`, htmlBuild)
